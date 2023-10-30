9892a1f8e419d76a59559918b2bf4479ab588f8c
Luisa Mathias <86033613+lumathias@users.noreply.github.com>
2023-10-29 21:28:40 -0300

    Update main.py

--- Codes/main.py
+++ Codes/main.py
@@ -3,9 +3,9 @@ import alimento
 import nutri
 
 # Exemplo de uso:
-pessoa = Paciente ('dany maos fofas', 76, 1.74, 27, "masculino", 5)
+pessoa = paciente.Paciente ('dany maos fofas', 76, 1.74, 27, "masculino", 5)
 
-tmb = Paciente.calcular_tmb(pessoa)
-tmb_final = fator_atividade(pessoa)
+tmb = paciente.Paciente.calcular_tmb(pessoa)
+tmb_final = paciente.fator_atividade(pessoa)
 print(f"TMB: {tmb}")
 print(f"TMB Final: {tmb_final}") 
