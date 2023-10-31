6ac55334f4fc4c940dc547e27c6d0bc94b769542
Luisa Mathias <86033613+lumathias@users.noreply.github.com>
2023-10-31 10:48:32 -0300

    code update

--- Codes/alimento.py
+++ Codes/alimento.py
@@ -1,6 +1,17 @@
-class Alimento:
-  def __init__(self, nome, calorias, gordura, proteina):
-    self.nome = nome
-    self.calorias = calorias
-    self.proteina = proteina
-    self.gordura = gordura
+import pandas as pd
+
+alimentos = pd.DataFrame()
+alimentos.columns = ['Nome', 'Calorias', 'Gorduras', 'Proteínas']
+
+def cadastrar_alimento():
+  alimentos['Nome'] = input('Alimento: ')
+  alimentos['Calorias'] = input('Calorias: ')
+  alimentos['Gordura'] = input('Gorduras: ')
+  alimentos['Proteína'] = input('Proteínas: ')
+
+nomes = alimentos['Nome']
+
+def remover_alimento(alimento):
+  for itesm in nomes:
+    if alimentos.Nome[item] == alimento:
+      alimentos = alimentos.drop(item)
